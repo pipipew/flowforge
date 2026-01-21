02a9e5abe015de0698e94b9ce35c23764d034349
pipipew <vonbravo2002@gmail.com>
2026-01-21 20:06:58 +0800

    Create index.ts
    Barrel export file for all goal components and subcomponents

---
+++ src/components/goals/index.ts
@@ -0,0 +1,10 @@
+// Goal Components
+export { GoalForm } from './GoalForm';
+export { GoalProgressCard } from './GoalProgressCard';
+export { GoalsList } from './GoalsList';
+
+// Achievement Components
+export { AchievementCard, AchievementsGrid } from './AchievementCard';
+
+// Leaderboard Components
+export { LeaderboardCard, LeaderboardsGrid } from './LeaderboardCard';
